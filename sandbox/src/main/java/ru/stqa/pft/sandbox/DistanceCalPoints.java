6b8ce485b0f15af15772efb391430273c2bb17ca
Molodkin Aleksey <a.molodkin@mail.msk>
2016-02-25 19:17:45 +0300

    добавил метод вычисления расстояния между точками Point()

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceCalPoints.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by a.molodkin on 25.02.2016.
+ */
+public class DistanceCalPoints {
+  public static void main(String[] args) {
+    Point p =new Point(0,0);
+    Point p1 =new Point(1,4);
+    Point p2 =new Point(5,10);
+    System.out.println("Расстояние между точками: "+"p1("+p1.x+","+p1.y+") и  p2("+p2.x+","+p2.y+")"+" = "+p.distance(p1,p2));
+
+  }
+
+
+}
+
--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,23 +6,27 @@ public class MyFirstProgram{
 		hello("world");
 		hello("user");
 		hello("Aleksey");
-		double l=5;
-		System.out.println("Площадь квадрата со стороной "+l+" = "+area(l));
 
-		double a=4;
-		double b=6;
-		System.out.println("Площадь прямоугольника со стороной "+a+" и "+b+" = "+area(a,b));
+		Square s=new Square(5);
+
+		System.out.println("Площадь квадрата со стороной "+s.l+" = "+s.area());
+
+		Rectangle r = new Rectangle(4,6);
+		System.out.println("Площадь прямоугольника со стороной "+r.a+" и "+r.b+" = "+r.area());
+
+		Point p1 =new Point(1,4);
+		Point p2 =new Point(5,10);
+		System.out.println("Расстояние между точками: "+"p1("+p1.x+","+p1.y+") и  p2("+p2.x+","+p2.y+")"+" = "+distance(p1,p2));
 	}
 
 	public static void hello(String somebody){
 		System.out.println("Hello, "+somebody+"!");
+
 	}
 
-	public static double area(double len){
-		return len*len;
+	public static double distance (Point p1, Point p2){
+		return Math.sqrt(Math.pow((p2.x-p1.x),2)+(Math.pow((p2.y-p1.y),2)));
 	}
 
-	public static double area(double a, double b){
-		return a*b;
-	}
+
 }
