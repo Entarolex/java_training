031e42d0f1c37d7338eecbf1d52ce43fed451773
Molodkin Aleksey <a.molodkin@mail.msk>
2016-02-26 16:19:49 +0300

    переопределил фунцию distance() в point.java как глобальную

--- sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceCalPoints.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceCalPoints.java
@@ -5,10 +5,9 @@ package ru.stqa.pft.sandbox;
  */
 public class DistanceCalPoints {
   public static void main(String[] args) {
-    Point p =new Point(0,0);
     Point p1 =new Point(1,4);
     Point p2 =new Point(5,10);
-    System.out.println("Расстояние между точками: "+"p1("+p1.x+","+p1.y+") и  p2("+p2.x+","+p2.y+")"+" = "+p.distance(p1,p2));
+    System.out.println("Расстояние между точками: "+"p1("+p1.x+","+p1.y+") и  p2("+p2.x+","+p2.y+")"+" = "+Point.distance(p1,p2));
 
   }
 
