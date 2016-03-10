0b18e0ce66cabf45d3dc69de98a5e066f83e01c8
EnTaroLex <molodkin352@gmail.com>
2016-03-10 12:55:02 +0300

    add GroupCreationTest.java

--- sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.sandbox;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+
+
 /**
  * Created by a.molodkin on 29.02.2016.
  */
@@ -10,6 +12,7 @@ public class SquareTests {
   @Test
   public void testArea(){
     Square s=new Square(5);
+    System.out.println();
     Assert.assertEquals(s.area(),25.0);
 
   }
