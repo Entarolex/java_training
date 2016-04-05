9a14cc5d123d4645e61a0343d4121d72f38067a4
entarolex <molodkin352@gmail.com>
2016-04-05 17:23:16 +0300

    l5_m6

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModifacationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModifacationTests.java
@@ -4,10 +4,11 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
 
 /**
  * Created by a.molodkin on 15.03.2016.
@@ -24,17 +25,15 @@ public class GroupModifacationTests extends TestBase {
 
   @Test
   public void testGroupModifacatoin(){
-    Set<GroupData> before = app.group().all();
+    Groups before = app.group().all();
     GroupData modifiedGroup = before.iterator().next();
     GroupData group= new GroupData()
             .withId(modifiedGroup.getId()).withName("test3").withHeader("test3").witnFooter("test3");
     app.group().modify(group);
-    Set<GroupData> after = app.group().all();
-    Assert.assertEquals(after.size(), before.size());
+    Groups after = app.group().all();
+    assertEquals(after.size(), before.size());
+    assertThat(after, equalTo(before.withOut(modifiedGroup).withAdded(group)));
 
-    before.remove(modifiedGroup);
-    before.add(group);
-    Assert.assertEquals(before,after);
   }
 
 
