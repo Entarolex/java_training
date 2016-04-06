86705330ecef0f6558169ef0390864f4c07234d9
entarolex <molodkin352@gmail.com>
2016-04-06 14:13:48 +0300

    homework11_l5 Задание №11: Реализовать проверки в fluent-стиле

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.util.HashSet;
 import java.util.List;
@@ -94,8 +95,8 @@ public class ContactHelper extends HelperBase {
     return wd.findElements(By.name("selected[]")).size();
   }
 
-   public Set<ContactData> all() {
-    Set<ContactData> contacts = new HashSet<ContactData>();
+   public Contacts all() {
+     Contacts contacts = new Contacts();
     List<WebElement> fio = wd.findElements(By.name("selected[]"));
 
     for (WebElement element : fio) {
