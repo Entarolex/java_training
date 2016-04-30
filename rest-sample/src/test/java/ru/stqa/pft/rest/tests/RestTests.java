7c84154fb2cc678af6645a8316b2763d0852da63
entarolex <molodkin352@gmail.com>
2016-05-01 02:14:59 +0300

    l9_h22 Задание №22: Реализовать интеграцию тестов с баг-трекером Bugify

--- rest-sample/src/test/java/ru/stqa/pft/rest/tests/RestTests.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/tests/RestTests.java
@@ -1,23 +1,15 @@
 package ru.stqa.pft.rest.tests;
 
 
-import biz.futureware.mantis.rpc.soap.client.IssueData;
-import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
-import com.google.gson.reflect.TypeToken;
-import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.message.BasicNameValuePair;
 import org.testng.annotations.Test;
-import ru.stqa.pft.rest.model.Project;
 
 import javax.xml.rpc.ServiceException;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import static org.testng.Assert.assertEquals;
 
@@ -25,73 +17,36 @@ import static org.testng.Assert.assertEquals;
  * Created by a.molodkin on 28.04.2016.
  */
 public class RestTests extends TestBase {
-  @Test
-  public void testGetIssues() throws IOException, ServiceException {
-    Set<Project> projects = getProjects();
-    Set<Issue> iss = getIssues();
-    System.out.println("Count issues: "+iss.size());
-    //List<Issue> issues = new Arrays.asList(iss.)
-    Set<Issue> issue = (Set<Issue>) iss.iterator().next();
-    for (Issue i: iss){
-
-      System.out.println("id " + i.getId());
-  //    System.out.println("Description " + i.getDescription());
-//      System.out.println("Subject " + i.getSubject());
-      System.out.println("state " + i.getState_name());//1- InProgress //2- Resolved
-      skipIfNotFixed(i.getId());
-      System.out.println("--");
+    @Test
+    public void testGetIssues() throws IOException, ServiceException {
+        Set<Issue> iss = getIssues();
+        System.out.println("Count issues: " + iss.size());
+        for (Issue i : iss) {
+            System.out.println("id " + i.getId());
+            System.out.println("state " + i.getState_name());
+            skipIfNotFixed(i.getId());
+            System.out.println("--");
+        }
     }
-}
 
 
- /* @Test
-  public void testCreateIssue() throws IOException {
-  Set<Issue>  oldIssue = getIssues();
-  Issue newIssue = new Issue().withSubject("Test issue").withDescription("New test issue");
-  int issueId = createIssue(newIssue);
-  Set<Issue> newIssues = getIssues();
-  oldIssue.add(newIssue.withId(issueId));
-  assertEquals(newIssues,oldIssue);
-}*/
+    //@Test
+    public void testCreateIssue() throws IOException {
+        Set<Issue> oldIssue = getIssues();
+        Issue newIssue = new Issue().withSubject("Test issue").withDescription("New test issue");
+        int issueId = createIssue(newIssue);
+        Set<Issue> newIssues = getIssues();
+        oldIssue.add(newIssue.withId(issueId));
+        assertEquals(newIssues, oldIssue);
+    }
 
-  public Set<Project> getProjects() throws IOException {
-    String projects = getExecutor().execute(Request.Get("http://demo.bugify.com/api/projects.json")).returnContent().asString();
-    System.out.println("projects"+projects);JsonElement parsed = new JsonParser().parse(projects);
-    JsonElement issues = parsed.getAsJsonObject().get("projects");
-    return new Gson().fromJson(issues,new TypeToken<Set<Issue>>(){}.getType());
-  }
-  private Set<Issue> getIssues() throws IOException {
-    String json = getExecutor().execute(Request.Get("http://demo.bugify.com/api/issues.json"))
-            .returnContent().asString();
-    //System.out.println(json1);
-//    String filters = getExecutor().execute(Request.Get("http://demo.bugify.com/api/filters/3/issues.json")).returnContent().asString();
-    //System.out.println(filters);
-
-    //String json = getExecutor().execute(Request.Get("http://demo.bugify.com/api/filters/0/issues.json")).returnContent().asString();
-    System.out.println(json);
-    JsonElement parsed = new JsonParser().parse(json);
-    JsonElement issues = parsed.getAsJsonObject().get("issues");
-    //JsonElement parsedF = new JsonParser().parse(filters);
-    //JsonElement filtersF = parsed.getAsJsonObject().get("filters");
-    //System.out.println("\nfiltersF "+new Gson().fromJson(filtersF,new TypeToken<Set<Issue>>(){}.getType()));
-    /*return new Gson().fromJson(issues,new TypeToken<Set<Issue>>(){}.getType());*/
-    IssueData[] iss= new Gson().fromJson(issues,new TypeToken<Set<Issue>>(){}.getType());
-    return  Arrays.asList(iss).stream()
-            .map((i) -> new Issue().withId(i.getId().intValue()).withStateName(i.getStatus().getName()))
-            .collect(Collectors.toSet());
-  }
-
-  private Executor getExecutor() {
-    return Executor.newInstance().auth("LSGjeU4yP1X493ud1hNniA==", "");
-  }
-
-  private int createIssue(Issue newIssue) throws IOException {
-    String json =  getExecutor().execute(Request.Post("http://demo.bugify.com/api/issues.json")
-            .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject())
-            ,new BasicNameValuePair("description", newIssue.getDescription())))
-            .returnContent().asString();
-    JsonElement parsed = new JsonParser().parse(json);
-    return parsed.getAsJsonObject().get("issue_id").getAsInt();
-  }
+    private int createIssue(Issue newIssue) throws IOException {
+        String json = getExecutor().execute(Request.Post("http://demo.bugify.com/api/issues.json")
+                .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject())
+                        , new BasicNameValuePair("description", newIssue.getDescription())))
+                .returnContent().asString();
+        JsonElement parsed = new JsonParser().parse(json);
+        return parsed.getAsJsonObject().get("issue_id").getAsInt();
+    }
 
 }
--- rest-sample/src/test/java/ru/stqa/pft/rest/tests/TestBase.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/tests/TestBase.java
@@ -1,6 +1,5 @@
 package ru.stqa.pft.rest.tests;
 
-import biz.futureware.mantis.rpc.soap.client.IssueData;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
@@ -11,8 +10,6 @@ import org.testng.SkipException;
 
 import javax.xml.rpc.ServiceException;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.rmi.RemoteException;
 import java.util.Set;
 
 /**
@@ -20,35 +17,43 @@ import java.util.Set;
  */
 public class TestBase {
 
-   public void skipIfNotFixed(int issueId) throws IOException, ServiceException {
-    if (isIssueOpen(issueId)) {
-      System.out.println("Issue # " + issueId + " is not fixed");
-      throw new SkipException("Ignored because of issue " + issueId);
-    } else {
-      System.out.println("Issue is fixed, U can test it");
+    public void skipIfNotFixed(int issueId) throws IOException, ServiceException {
+        if (isIssueOpen(issueId)) {
+            System.out.println("Issue # " + issueId + " is not fixed");
+            throw new SkipException("Ignored because of issue " + issueId);
+        } else {
+            System.out.println("Issue is fixed, U can test it");
+        }
     }
-  }
 
 
-  public boolean isIssueOpen(int issueId) throws IOException, ServiceException {
-    String json = getExecutor().execute(Request.Get("http://demo.bugify.com/api/issues.json"))
-            .returnContent().asString();
-    JsonElement parsed = new JsonParser().parse(json);
-    JsonElement issues = parsed.getAsJsonObject().get("issues");
-
-    return false;
-
-    /* MantisConnectPortType mc = app.soap().getMantisConnect();
-    IssueData issue = mc.mc_issue_get(app.getProperty("web.adminLogin"), app.getProperty("web.adminPassword"), BigInteger.valueOf(issueId));
-    if (issue.getState().getName().equals("resolved") || issue.getState().getName().equals("closed")) {
-      return false;
+    public boolean isIssueOpen(int issueId) throws IOException {
+        return !getIssueById(issueId).getState_name().equals("Resolved");
     }
-    return true;*/
-  }
 
-  private Executor getExecutor() {
-    return Executor.newInstance().auth("LSGjeU4yP1X493ud1hNniA==", "");
-  }
+    public Issue getIssueById(int id) throws IOException {
+        Issue issue = new Issue();
+        getIssues()
+                .stream().findFirst().map
+                ((i) -> issue.withId(i.getId()).withSubject(i.getSubject())
+                        .withDescription(i.getDescription())
+                        .withStateName(i.getState_name())
+                );
+        return issue;
+    }
+
+    public Set<Issue> getIssues() throws IOException {
+        String json = getExecutor().execute(Request.Get("http://demo.bugify.com/api/issues.json"))
+                .returnContent().asString();
+        JsonElement parsed = new JsonParser().parse(json);
+        JsonElement issues = parsed.getAsJsonObject().get("issues");
+        return new Gson().fromJson(issues, new TypeToken<Set<Issue>>() {
+        }.getType());
+    }
+
+    public Executor getExecutor() {
+        return Executor.newInstance().auth("LSGjeU4yP1X493ud1hNniA==", "");
+    }
 }
 
 
