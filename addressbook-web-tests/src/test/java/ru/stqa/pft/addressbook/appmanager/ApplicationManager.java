66245f9373c1dce847ef124c9d0c379b81add431
entarolex <molodkin352@gmail.com>
2016-05-01 20:49:06 +0300

    l10_m2

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,84 +1,87 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
-import ru.stqa.pft.addressbook.model.ContactData;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
-import java.util.StringTokenizer;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by a.molodkin on 10.03.2016.
  */
 public class ApplicationManager {
-  private final Properties properties;
-  WebDriver wd;
-  private SessionHelper sessionHelper;
-  private NavigationHelper navigationHelper;
-  private GroupHelper groupHelper;
-  private ContactHelper contactHelper;
-  private String browser;
-  private DbHelper dbHelper;
+    private final Properties properties;
+    WebDriver wd;
+    private SessionHelper sessionHelper;
+    private NavigationHelper navigationHelper;
+    private GroupHelper groupHelper;
+    private ContactHelper contactHelper;
+    private String browser;
+    private DbHelper dbHelper;
 
-  public ApplicationManager(String browser) {
-    this.browser = browser;
-    properties = new Properties();
-  }
-
-
-  public void init() throws IOException {
-    String target = System.getProperty("target", "local");
-
-    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
-
-    dbHelper = new DbHelper();
-
-    if (browser.equals(BrowserType.FIREFOX)){
-      wd=new FirefoxDriver();
-    }else if (browser.equals(BrowserType.CHROME)){
-      wd=new ChromeDriver();
-    }else if (browser.equals(BrowserType.IE)){
-      wd=new InternetExplorerDriver();
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+        properties = new Properties();
     }
-    //wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    wd.get(properties.getProperty("web.baseUrl"));
-    groupHelper = new GroupHelper(wd);
-    navigationHelper = new NavigationHelper(wd);
-    sessionHelper=new SessionHelper(wd);
-    sessionHelper.login(properties.getProperty("web.adminLogin"),properties.getProperty("web.adminPassword"));
-    contactHelper=new ContactHelper(wd);
 
 
-  }
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+        dbHelper = new DbHelper();
+
+        if ("".equals(properties.getProperty("selenium.server"))) {
+            if (browser.equals(BrowserType.FIREFOX)) {
+                wd = new FirefoxDriver();
+            } else if (browser.equals(BrowserType.CHROME)) {
+                wd = new ChromeDriver();
+            } else if (browser.equals(BrowserType.IE)) {
+                wd = new InternetExplorerDriver();
+            }
+        } else {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
+        }
+        //wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        wd.get(properties.getProperty("web.baseUrl"));
+        groupHelper = new GroupHelper(wd);
+        navigationHelper = new NavigationHelper(wd);
+        sessionHelper = new SessionHelper(wd);
+        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
+        contactHelper = new ContactHelper(wd);
 
 
+    }
 
-  public void stop() {
-    wd.quit();
-  }
 
-  public GroupHelper group() {
-    return groupHelper;
-  }
+    public void stop() {
+        wd.quit();
+    }
 
-  public NavigationHelper goTo() {
-    return navigationHelper;
-  }
+    public GroupHelper group() {
+        return groupHelper;
+    }
 
-  public ContactHelper contact() {
-    return contactHelper;
-  }
+    public NavigationHelper goTo() {
+        return navigationHelper;
+    }
 
-  public DbHelper db(){
-    return dbHelper;
-  }
+    public ContactHelper contact() {
+        return contactHelper;
+    }
+
+    public DbHelper db() {
+        return dbHelper;
+    }
 }
